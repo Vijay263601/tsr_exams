e76ac296596f485c6398b4047747ca441e2f3e7a
Vijay <vijay263601@gmail.com>
2026-01-03 21:48:56 +0530

    Update config.js

--- common/config.js
+++ common/config.js
@@ -1,1 +1,6 @@
+// GitHub Pages base path
+const BASE_PATH = "/tsr_exams";
+
+// Backend API
+
 const API_URL = "https://script.google.com/macros/s/AKfycbzJa9u_OfD-yQi7WHPK6iSSMYeE49yj1XiewMI5RZ4UOPqcDXYwoMiN2NbQ4GwLcZ_P/exec";
