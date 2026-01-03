3a606fcbe58a646c2855e2c16137b57cbecd84ed
Vijay <vijay263601@gmail.com>
2026-01-03 22:39:56 +0530

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,6 @@ function startExam() {
     return;
   }
 
-<<<<<<< HEAD
   sessionStorage.setItem(
     "student",
     JSON.stringify({
@@ -22,15 +21,4 @@ function startExam() {
   window.location.href =
     BASE_PATH + "/exam/exam.html?examId=" +
     encodeURIComponent(examId);
-=======
-  // Save student info for exam page
-  sessionStorage.setItem(
-    "student",
-    JSON.stringify({ name, roll, email })
-  );
-
-  // Redirect to exam
-     window.location.href =
-    "../tsr_exams/exam/exam.html?examId=" + encodeURIComponent(examId);
->>>>>>> aa019b855f74ef2086072f05521e22541c972f51
 }
